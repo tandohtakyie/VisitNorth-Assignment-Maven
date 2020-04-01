897a6b13178cc90433bf878feca76f03243fe132
Gabriel <tandohtakyie@hotmail.com>
2020-04-01 01:30:27 +0100

    Added a little design to the manager screen

--- src/main/java/Database/DatabaseConnection.java
+++ src/main/java/Database/DatabaseConnection.java
@@ -7,18 +7,18 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static String databaseUsername = "root";
     // gabriel password
-    //private static String databasePassword = "admin";
+    private static String databasePassword = "admin";
     // jack password
-    private static String databasePassword = "GoodGameMySQL2798";
+   // private static String databasePassword = "GoodGameMySQL2798";
     private static String databaseName = "visitnorth";
 
     public static Connection connectDB() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             // gabriel connect
-            //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, databaseUsername, databasePassword);
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, databaseUsername, databasePassword);
             // jack connect
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/"+ databaseName +"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", databaseUsername, databasePassword);
+           // Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/"+ databaseName +"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", databaseUsername, databasePassword);
             return conn;
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
--- src/main/java/View/ManagerScreen.java
+++ src/main/java/View/ManagerScreen.java
@@ -1,16 +1,35 @@
 package View;
-
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JLayeredPane;
+import java.awt.CardLayout;
 
 public class ManagerScreen extends JFrame {
-
+	
+	int xx,xy;
+	
+	private static ManagerScreen managerFrame;
+	
 	private JPanel contentPane;
+	private JLayeredPane layeredPane;
+	
+	private JPanel homePanel;
+	private JPanel citiesPanel;
+	private JPanel schedulePanel;
+	private JPanel settingsPanel;
 
 	/**
 	 * Launch the application.
@@ -19,8 +38,9 @@ public class ManagerScreen extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ManagerScreen frame = new ManagerScreen();
-					frame.setVisible(true);
+					managerFrame = new ManagerScreen();
+					managerFrame.setUndecorated(true);
+					managerFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,14 +53,252 @@ public class ManagerScreen extends JFrame {
 	 */
 	public ManagerScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1077, 755);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				xx = e.getX();
+				xy = e.getY();
+			}
+		});
+		contentPane.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent arg0) {
+				int x = arg0.getXOnScreen();
+				int y = arg0.getYOnScreen();
+				ManagerScreen.this.setLocation(x - xx, y - xy);
+			}
+		});
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Manager");
-		contentPane.add(lblNewLabel, BorderLayout.CENTER);
+		JPanel panelBG = new JPanel();
+		panelBG.setBackground(new Color(255, 255, 255));
+		panelBG.setBounds(0, 0, 1073, 760);
+		contentPane.add(panelBG);
+		panelBG.setLayout(null);
+		
+		JPanel sidePanel = new JPanel();
+		sidePanel.setBackground(new Color(102, 51, 153));
+		sidePanel.setBounds(0, 0, 283, 760);
+		panelBG.add(sidePanel);
+		sidePanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(283, 0, 790, 760);
+		panelBG.add(panel);
+		panel.setLayout(null);
+		
+		layeredPane = new JLayeredPane();
+		layeredPane.setBackground(new Color(255, 255, 255));
+		layeredPane.setBounds(0, 105, 790, 655);
+		panel.add(layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+		
+		JPanel btnHomePanel = new JPanel();
+		btnHomePanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				switchPanel(homePanel);
+			}
+		});
+		btnHomePanel.setBackground(new Color(153, 102, 204));
+		btnHomePanel.setBounds(0, 105, 283, 58);
+		sidePanel.add(btnHomePanel);
+		btnHomePanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(22, 11, 40, 33);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(ManagerScreen.class.getResource("/Images/icons8_home_20px.png")));
+		btnHomePanel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Home");
+		lblNewLabel_1.setBounds(95, 14, 78, 33);
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1.setFont(new Font("Candara", Font.BOLD, 19));
+		btnHomePanel.add(lblNewLabel_1);
+		
+		JPanel btnRoutePanel = new JPanel();
+		btnRoutePanel.setLayout(null);
+		btnRoutePanel.setBackground(new Color(102, 51, 153));
+		btnRoutePanel.setBounds(0, 164, 283, 58);
+		btnRoutePanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				switchPanel(citiesPanel);
+			}
+		});
+		sidePanel.add(btnRoutePanel);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(ManagerScreen.class.getResource("/Images/icons8_tour_bus_20px.png")));
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setBounds(22, 11, 40, 33);
+		btnRoutePanel.add(label);
+		
+		JLabel lblCities = new JLabel("Route");
+		lblCities.setHorizontalAlignment(SwingConstants.LEFT);
+		lblCities.setForeground(Color.WHITE);
+		lblCities.setFont(new Font("Candara", Font.BOLD, 19));
+		lblCities.setBounds(95, 14, 78, 33);
+		btnRoutePanel.add(lblCities);
+		
+		JPanel btnSchedulePanel = new JPanel();
+		btnSchedulePanel.setLayout(null);
+		btnSchedulePanel.setBackground(new Color(102, 51, 153));
+		btnSchedulePanel.setBounds(0, 222, 283, 58);
+		btnSchedulePanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				switchPanel(schedulePanel);
+			}
+		});
+		sidePanel.add(btnSchedulePanel);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon(ManagerScreen.class.getResource("/Images/icons8_grid_3_20px.png")));
+		label_2.setHorizontalAlignment(SwingConstants.CENTER);
+		label_2.setBounds(22, 11, 40, 33);
+		btnSchedulePanel.add(label_2);
+		
+		JLabel lblSchedules = new JLabel("Schedules");
+		lblSchedules.setHorizontalAlignment(SwingConstants.LEFT);
+		lblSchedules.setForeground(Color.WHITE);
+		lblSchedules.setFont(new Font("Candara", Font.BOLD, 19));
+		lblSchedules.setBounds(95, 14, 97, 33);
+		btnSchedulePanel.add(lblSchedules);
+		
+		JPanel btnSettingsPanel = new JPanel();
+		btnSettingsPanel.setLayout(null);
+		btnSettingsPanel.setBackground(new Color(102, 51, 153));
+		btnSettingsPanel.setBounds(0, 279, 283, 58);
+		btnSettingsPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				switchPanel(settingsPanel);
+			}
+		});
+		sidePanel.add(btnSettingsPanel);
+		
+		JLabel label_4 = new JLabel("");
+		label_4.setIcon(new ImageIcon(ManagerScreen.class.getResource("/Images/icons8_settings_20px.png")));
+		label_4.setHorizontalAlignment(SwingConstants.CENTER);
+		label_4.setBounds(22, 11, 40, 33);
+		btnSettingsPanel.add(label_4);
+		
+		JLabel lblSettings = new JLabel("Settings");
+		lblSettings.setHorizontalAlignment(SwingConstants.LEFT);
+		lblSettings.setForeground(Color.WHITE);
+		lblSettings.setFont(new Font("Candara", Font.BOLD, 19));
+		lblSettings.setBounds(95, 14, 78, 33);
+		btnSettingsPanel.add(lblSettings);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setLayout(null);
+		panel_5.setBackground(new Color(87, 41, 133));
+		panel_5.setBounds(0, 702, 283, 58);
+		sidePanel.add(panel_5);
+		
+		JLabel lblLogout = new JLabel("");
+		lblLogout.setIcon(new ImageIcon(ManagerScreen.class.getResource("/Images/icons8_sign_out_20px.png")));
+		lblLogout.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogout.setBounds(22, 11, 40, 33);
+		panel_5.add(lblLogout);
+		
+		JLabel lblJohnDoe = new JLabel("John Doe");
+		lblJohnDoe.setHorizontalAlignment(SwingConstants.LEFT);
+		lblJohnDoe.setForeground(Color.WHITE);
+		lblJohnDoe.setFont(new Font("Candara", Font.BOLD, 19));
+		lblJohnDoe.setBounds(95, 14, 78, 33);
+		panel_5.add(lblJohnDoe);
+		
+		JLabel lblVisitNorth = new JLabel("Visit North");
+		lblVisitNorth.setBounds(39, 39, 111, 33);
+		sidePanel.add(lblVisitNorth);
+		lblVisitNorth.setHorizontalAlignment(SwingConstants.LEFT);
+		lblVisitNorth.setForeground(Color.WHITE);
+		lblVisitNorth.setFont(new Font("Candara", Font.BOLD, 19));
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBounds(29, 69, 227, 3);
+		sidePanel.add(panel_6);
+		
+		
+		
+		JLabel lblNewLabel_2 = new JLabel("Luxury coaches for  tourists in and North East England.");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Candara", Font.PLAIN, 17));
+		lblNewLabel_2.setBounds(10, 41, 770, 26);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblClose = new JLabel("");
+		lblClose.setVerticalAlignment(SwingConstants.TOP);
+		lblClose.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblClose.setIcon(new ImageIcon(ManagerScreen.class.getResource("/Images/icons8_multiply_20px_1.png")));
+		lblClose.setBounds(757, 0, 33, 32);
+		lblClose.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.exit(0);
+			}
+		});
+		panel.add(lblClose);
+		
+		
+		
+		
+		homePanel = new JPanel();
+		homePanel.setBackground(new Color(255, 255, 255));
+		layeredPane.add(homePanel, "name_20092475988800");
+		homePanel.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("Home");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_3.setBounds(95, 89, 110, 41);
+		homePanel.add(lblNewLabel_3);
+		
+		citiesPanel = new JPanel();
+		citiesPanel.setBackground(new Color(255, 255, 255));
+		layeredPane.add(citiesPanel, "name_20101062289600");
+		citiesPanel.setLayout(null);
+		
+		JLabel lblCity = new JLabel("City");
+		lblCity.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblCity.setBounds(126, 137, 110, 41);
+		citiesPanel.add(lblCity);
+		
+		schedulePanel = new JPanel();
+		schedulePanel.setBackground(new Color(255, 255, 255));
+		layeredPane.add(schedulePanel, "name_20107772105900");
+		schedulePanel.setLayout(null);
+		
+		JLabel lblSchedule = new JLabel("Schedule");
+		lblSchedule.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblSchedule.setBounds(166, 144, 110, 41);
+		schedulePanel.add(lblSchedule);
+		
+		settingsPanel = new JPanel();
+		settingsPanel.setBackground(new Color(255, 255, 255));
+		layeredPane.add(settingsPanel, "name_20239578953600");
+		settingsPanel.setLayout(null);
+		
+		JLabel lblSettings_1 = new JLabel("Settings");
+		lblSettings_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblSettings_1.setBounds(76, 171, 110, 41);
+		settingsPanel.add(lblSettings_1);
+		
 	}
-
+	
+	public void switchPanel(JPanel panel) {
+		layeredPane.removeAll();
+		layeredPane.add(panel);
+		layeredPane.repaint();
+		layeredPane.revalidate();
+	}
 }
